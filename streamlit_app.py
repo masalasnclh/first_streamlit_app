f2eb999ed2f734b1779121af893f16d1401be646
masalasnclh <138933160+masalasnclh@users.noreply.github.com>
2023-07-07 17:07:29 -0600

    Update streamlit_app.py
    fruit choices feature added

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,9 +23,11 @@ sl.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 sl.header("Fruityvice Fruit Advice!")
+fruit_choice = sl.text_input('What fruit would you like information about?', 'Kiwi')
+sl.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # sl.text(fruityvice_response.json()) #just displays json object to screen
 
