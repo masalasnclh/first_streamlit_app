0b2daad05ce65b512cd71b726ccce0772e571266
masalasnclh <138933160+masalasnclh@users.noreply.github.com>
2023-07-07 14:31:55 -0600

    Update streamlit_app.py
    aliased streamlit as sl
    aliased pandas as pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
-import streamlit
+import streamlit as sl
 
-streamlit.title( 'My Parents New Healthy Diner' )
+sl.title( 'My Parents New Healthy Diner' )
 
-streamlit.header( 'Breakfast Favorites' )
-streamlit.text( '🥣 Omega 3 & Blueberry Oatmeal' )
-streamlit.text( '🥗 Kale, Spinach & Rocket Smoothie' )
-streamlit.text( '🐔 Hard-Boiled Free-Range Egg' )
-streamlit.text( '🥑🍞 Avocado Toast' )
+sl.header( 'Breakfast Favorites' )
+sl.text( '🥣 Omega 3 & Blueberry Oatmeal' )
+sl.text( '🥗 Kale, Spinach & Rocket Smoothie' )
+sl.text( '🐔 Hard-Boiled Free-Range Egg' )
+sl.text( '🥑🍞 Avocado Toast' )
 
-streamlit.header( '🍌🥭 Build Your Own Fruit Smoothie 🥝🍇' )
+sl.header( '🍌🥭 Build Your Own Fruit Smoothie 🥝🍇' )
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sl.dataframe(my_fruit_list)
